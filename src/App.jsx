d57ad9808f19c8959eaa034d37c269c86b3fa5a1
KENNAH CEO <kennah@kali>
2025-03-22 16:35:24 +0300

    add parent-dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import './styles/App.css'
-import Dashboard from "./components/teachersDashboard/pages/Dashboard"
-import Classes from "./components/teachersDashboard/pages/Classes"
-import Events from "./components/teachersDashboard/pages/Events"
-import Exams from "./components/teachersDashboard/pages/Exams"
-import NoticeBoard from "./components/teachersDashboard/pages/NoticeBoard"
-import Routine from "./components/teachersDashboard/pages/Routine"
-import Students from "./components/teachersDashboard/pages/Students"
-import Subjects from "./components/teachersDashboard/pages/Subjects"
+import './styles/App.css';
+import Dashboard from "./components/teachersDashboard/pages/Dashboard";
+import Classes from "./components/teachersDashboard/pages/Classes";
+import Events from "./components/teachersDashboard/pages/Events";
+import Exams from "./components/teachersDashboard/pages/Exams";
+import NoticeBoard from "./components/teachersDashboard/pages/NoticeBoard";
+import Routine from "./components/teachersDashboard/pages/Routine";
+import Students from "./components/teachersDashboard/pages/Students";
+import Subjects from "./components/teachersDashboard/pages/Subjects";
+import Navbar from './components/parentDashboard/domain/Navbar';
+import Dashbod from './components/parentDashboard/domain/Dashboard1'; // Corrected import
+import StudentResults from './components/parentDashboard/domain/StudentResults';
+import Talks from './components/parentDashboard/domain/Talks';
 
-import HomePage from './components/Home'
+import HomePage from './components/Home';
 import Login from "./components/Login";
 import SignUp from "./components/Signup";
 import ErrorPage from "./components/ErrorPage";
@@ -20,95 +24,106 @@ import StudentDashboard from "./components/studentDashboard/StudentDashboard";
 import TeachersDashboard from "./components/teachersDashboard/TeachersDashboard";
 import ForgotPassword from "./components/ForgotPassword";
 
-
 function App() {
-  
   const router = createBrowserRouter([
-    {path:'/',
-      element: <HomePage/>,
-      errorElement: <ErrorPage/>,
+    {
+      path: '/',
+      element: <HomePage />,
+      errorElement: <ErrorPage />,
       children: [
-        {index: true,
-          element: <Description/>
-        }
-        
-      ]
-    },
-    {path: '/signIn',
-      element:<Login/>
+        {
+          index: true,
+          element: <Description />,
+        },
+      ],
     },
     {
-      path:'/forgotPassword',
-      element:<ForgotPassword/>
+      path: '/signIn',
+      element: <Login />,
     },
-    
-    {path: '/student',
-      element: <StudentDashboard/>
+    {
+      path: '/forgotPassword',
+      element: <ForgotPassword />,
+    },
+
+    {
+      path: '/student',
+      element: <StudentDashboard />,
     },
     {
       path: '/teacher',
-      element: <TeachersDashboard/>,
-      children:[
-        {index: true,
-          element:<Dashboard/>
-        }, 
+      element: <TeachersDashboard />,
+      children: [
+        {
+          index: true,
+          element: <Dashboard />,
+        },
         {
           path: "dashboard",
-          element:<Dashboard/>
-
+          element: <Dashboard />,
         },
         {
           path: "students",
-          element:<Students/>
-
+          element: <Students />,
         },
         {
           path: "subjects",
-          element:<Subjects/>
-
+          element: <Subjects />,
         },
         {
           path: "classes",
-          element:<Classes/>
-
+          element: <Classes />,
         },
         {
           path: "routine",
-          element:<Routine/>
-
+          element: <Routine />,
         },
         {
           path: "exams",
-          element:<Exams/>
-
+          element: <Exams />,
         },
         {
           path: "notice",
-          element:<NoticeBoard/>
-
+          element: <NoticeBoard />,
         },
-        
+
         {
           path: "events",
-          element:<Events/>
-
+          element: <Events />,
         },
-      ]
+      ],
     },
-   
-    {path: '/signUp',
-      element: <SignUp/>
-    }
-  ])
-  
+
+    {
+      path: "/parent",
+      element: (
+        <div>
+          <Navbar onLogout={() => console.log("logout")} userName={"Kennah"} />
+          <Outlet />
+        </div>
+      ),
+      children: [
+        { index: true, element: <Dashbod /> },
+        { path: "dashboard", element: <Dashbod /> },
+        { path: "results", element: <StudentResults /> },
+        { path: "talks", element: <Talks /> },
+      ],
+    },
+
+    {
+      path: '/signUp',
+      element: <SignUp />,
+    },
+  ]
+  );
+
   const queryClient = new QueryClient();
- 
+
   return (
-    <QueryClientProvider client={queryClient} >
-      <RouterProvider router={router}/>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
     </QueryClientProvider>
-    
-  )
+  );
 }
 
-export default App
+export default App;
